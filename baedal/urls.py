fd05704efb2fff9882886b6a41ef81bf2ce10666
Jaehyeon Park <sawogus29@gmail.com>
2023-03-01 18:57:04 +0900

    --no-edit

--- baedal/urls.py
+++ baedal/urls.py
@@ -5,4 +5,6 @@ from . import views
 app_name = 'baedal' # namespace
 urlpatterns = [
   path('', views.index, name = 'index'),
+  path('customer/signin', views.customer_signin, name='customer_signin'),
+  path('customer/signup', views.customer_signup, name='customer_signup'),
 ]
--- baedal/views.py
+++ baedal/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'baedal/index.html', {})
+
+def customer_signin(request):
+    return render(request, 'baedal/customer_signin.html', {})
+
+def customer_signup(request):
+    return render(request, 'baedal/customer_signup.html', {})
