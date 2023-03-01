70152b3f9e50e8c50c8dbdf6f916f6b1dde00f57
Jaehyeon Park <sawogus29@gmail.com>
2023-03-01 19:33:16 +0900

    refactor(model): Change TextChoices to Korean

--- baedal/models.py
+++ baedal/models.py
@@ -5,7 +5,7 @@ class Restaurant(models.Model):
     username = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
     display_name = models.CharField(max_length=50)
-    category = models.CharField(choices=models.TextChoices('Category', 'Chicken Pizza Chinese Korean').choices, max_length=50)
+    category = models.CharField(choices=models.TextChoices('Category', '치킨 중식 한식').choices, max_length=50)
 
 class Customer(models.Model):
     username = models.CharField(max_length=50)
@@ -22,7 +22,7 @@ class Purchase(models.Model):
     restaurant = models.ForeignKey(Restaurant, on_delete=models.SET_NULL, null=True)
     customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
     total_price = models.IntegerField()
-    status = models.CharField(choices=models.TextChoices('StatusType', 'Pending Accepted Rejected').choices, max_length=50)
+    status = models.CharField(choices=models.TextChoices('StatusType', '대기 접수 반려').choices, max_length=50)
     created_date = models.DateTimeField()
 
 class PurchaseMenu(models.Model):
