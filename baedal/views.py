e02329e80f5dadca2bd2d5d8d12b9ed0ec2552f3
Jaehyeon Park <sawogus29@gmail.com>
2023-03-01 20:36:31 +0900

    feat(customer): Add signup

--- baedal/views.py
+++ baedal/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from .models import Restaurant
+from django.shortcuts import render, redirect
+from django.contrib.auth.hashers import make_password, check_password
+
+from .models import Customer, Restaurant
 
 # Create your views here.
 def index(request):
@@ -10,7 +12,26 @@ def customer_signin(request):
     return render(request, 'baedal/customer_signin.html', {})
 
 def customer_signup(request):
-    return render(request, 'baedal/customer_signup.html', {})
+    context = {}
+    if request.method == "POST" :
+        username = request.POST.get('username') 
+        # happy path
+        if not Customer.objects.filter(username=username):
+            # store Customer to DB
+            customer = Customer(
+                username=username, 
+                password=make_password(request.POST.get('password')), 
+                phone=request.POST.get('phone'), 
+                address=request.POST.get('address'), 
+            )
+            customer.save()
+
+            return redirect('baedal:customer_signin')
+        
+        # failure: username already exist
+        context['error'] = "이미 존재하는 ID입니다"
+
+    return render(request, 'baedal/customer_signup.html', context)
 # ============================================
 
 # =========== Restaurant =====================
